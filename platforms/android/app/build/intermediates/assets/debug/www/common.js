4eee284132ed92ee1f7f26d35a67feebcf60bf02
Daniel Zhang <jmzwcn@gmail.com>
2018-10-16 20:50:04 +0800

    build.
    Signed-off-by: Daniel Zhang <jmzwcn@gmail.com>

--- platforms/android/app/build/intermediates/assets/debug/www/common.js
+++ platforms/android/app/build/intermediates/assets/debug/www/common.js
@@ -786,7 +786,7 @@ var NewsPageModule = /** @class */ (function () {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<ion-header>\n  <ion-toolbar>\n    <ion-title>News</ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content>\n  <ion-slides pager=\"true\">\n    <ion-slide *ngFor=\"let article of data?.articles\" (click)=\"onGotoNewsDetail(article)\">\n      <!-- <ion-card *ngFor=\"let article of data?.articles\" (click)=\"onGotoNewsDetail(article)\"> -->\n      \n      <ion-card-content>\n          <p>\n              <ion-img [src]=\"article.urlToImage\"></ion-img>\n            </p>\n        <ion-card-title>{{article.title}}</ion-card-title>\n        <p>{{article.description}}</p>\n      </ion-card-content>\n      <!-- </ion-card> -->\n    </ion-slide>\n  </ion-slides>\n\n\n</ion-content>"
+module.exports = "<ion-header>\n  <ion-toolbar>\n    <ion-title>News</ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content>\n  <ion-slides pager=\"true\">\n    <ion-slide *ngFor=\"let article of data?.articles\" (click)=\"onGotoNewsDetail(article)\">\n      <ion-card>      \n      <ion-card-content>\n          <p>\n              <ion-img [src]=\"article.urlToImage\"></ion-img>\n            </p>\n        <ion-card-title>{{article.title}}</ion-card-title>\n        <p>{{article.content}}</p>\n      </ion-card-content>\n      </ion-card>\n    </ion-slide>\n  </ion-slides>\n\n\n</ion-content>\n"
 
 /***/ }),
 
@@ -834,7 +834,7 @@ var NewsPage = /** @class */ (function () {
     NewsPage.prototype.ngOnInit = function () {
         var _this = this;
         this.newsService
-            .getData('top-headlines?country=us')
+            .getData('top-headlines?language=en')
             .subscribe(function (data) {
             console.log(data);
             _this.data = data;
--- platforms/android/app/src/main/assets/www/main.js
+++ platforms/android/app/src/main/assets/www/main.js
@@ -1917,7 +1917,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/jmzwcn/work/src/github.com/bestNews/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /home/jmzwcn/work/src/github.com/bestnews/src/main.ts */"./src/main.ts");
 
 
 /***/ })
